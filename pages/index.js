1a0cf05257b90f73abf62de4c48e449b20110604
João Palmeiro <joaommpalmeiro@gmail.com>
2022-10-25 18:34:13 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,3 +5,8 @@ export default function Home() {
 
 // https://nextjs.org/docs/basic-features/data-fetching/get-static-props#when-should-i-use-getstaticprops
 // https://github.com/vercel/next.js/blob/canary/examples/cms-storyblok/pages/index.js
+// https://nextjs.org/docs/basic-features/data-fetching/get-static-props#using-getstaticprops-to-fetch-data-from-a-cms
+// https://nextjs.org/docs/basic-features/data-fetching/incremental-static-regeneration
+// https://github.com/PedroEdu6786/personal-blog/blob/develop/src/lib/api.ts#L16
+// https://www.storyblok.com/docs/api/content-delivery
+export async function getStaticProps() {}
